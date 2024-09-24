8962f8929cf0d4908e1dcf303d3a3cd47dae6b90
Vinaykrish25 <vinaykrish2002@gmail.com>
2024-09-24 14:10:49 +0530

    Updated

--- src/Components/Api.js
+++ src/Components/Api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const Api = axios.create({
-    baseURL: 'https://notes-app-backend-gamma-jet.vercel.app/',
+    baseURL: 'https://notes-app-backend-flax.vercel.app',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json',
