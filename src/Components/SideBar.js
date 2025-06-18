169765b90b77a1169baf470953c925209bb3a655
Vinaykrish25 <vinaykrish2002@gmail.com>
2025-06-19 00:56:30 +0530

    updated sidebar.js

--- src/Components/SideBar.js
+++ src/Components/SideBar.js
@@ -1,40 +1,51 @@
-import React from 'react'
-import { Link, useNavigate } from "react-router-dom"
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { FaRegLightbulb } from "react-icons/fa";
 import { MdLogout } from "react-icons/md";
 import { TiUserAdd } from "react-icons/ti";
 import { FaRegTrashAlt } from "react-icons/fa";
-import "./Styles/SideBar.css"
-import Api from './Api';
-import axios from 'axios';
+import "./Styles/SideBar.css";
+import Api from "./Api";
+import axios from "axios";
 
 const SideBar = () => {
-
   const navigate = useNavigate();
 
   const handleLogout = async () => {
-      try {
-          // const response = await axios.post('https://notes-app-backend-five-gold.vercel.app/users/logout', {}, { withCredentials: true });
-          const response = await Api.post('/users/logout', {}, { withCredentials: true });
-          navigate('/'); 
-          console.log(response)
-      } catch (err) {
-          console.error('Error logging out:', err);
-      }
+    try {
+      // const response = await axios.post('https://notes-app-backend-five-gold.vercel.app/users/logout', {}, { withCredentials: true });
+      const response = await Api.post(
+        "/users/logout",
+        {},
+        { withCredentials: true }
+      );
+      localStorage.clear(); // if you store anything
+      window.location.reload(); // force fresh load to remove stale cookie session
+      navigate("/");
+      console.log(response);
+    } catch (err) {
+      console.error("Error logging out:", err);
+    }
   };
 
   return (
-    <div className='sidebar-container'>
-      <Link to="/signup"><div className='sign'>
-        <h1><TiUserAdd /></h1>
-        <h3>Sign Up</h3>
-      </div></Link>
-      <div className='logout' onClick={handleLogout}>
-        <h1><MdLogout /></h1>
+    <div className="sidebar-container">
+      <Link to="/signup">
+        <div className="sign">
+          <h1>
+            <TiUserAdd />
+          </h1>
+          <h3>Sign Up</h3>
+        </div>
+      </Link>
+      <div className="logout" onClick={handleLogout}>
+        <h1>
+          <MdLogout />
+        </h1>
         <h3>Logout</h3>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default SideBar
+export default SideBar;
